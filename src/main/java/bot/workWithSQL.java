83d9c3737b0f5e08c2abbbae43083c4a29f6f950
Карина Лапицкая <karina.lapitskaya05@mail.ru>
2024-11-14 21:50:03 +0500

    Revert "исправлена работа с sql"
    This reverts commit ae26d4005477973295703b693d438f3df37de848.

--- src/main/java/bot/workWithSQL.java
+++ src/main/java/bot/workWithSQL.java
@@ -1,34 +1,36 @@
 package bot;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+
 import java.sql.*;
 public  class workWithSQL {
     public static Connection connection;
-    public static PreparedStatement statement;
+    public static Statement statement;
     public ResultSet resultSet;
+    public String value;
     public String URL;
-    public String USERNAME;
-    public String PASSWORD;
-    public String TABLE;
+    public  String USERNAME;
+    public  String PASSWORD;
+    public  String TABLE;
 
-    public workWithSQL(String url, String username, String password, String table) {
-        this.URL = url;
-        this.USERNAME = username;
-        this.PASSWORD = password;
-        this.TABLE = table;
-    }
+     public workWithSQL(String url, String username, String password, String table) {
+         this.URL= url;
+         this.USERNAME = username;
+         this.PASSWORD = password ;
+         this.TABLE = table ;
+     }
+    private void StartConnection(String SQL) throws SQLException {
+        connection = DriverManager.getConnection(URL,USERNAME,PASSWORD);
+        statement = connection.createStatement();
+        resultSet = statement.executeQuery(SQL);
 
-    private void StartConnection() throws SQLException {
-        connection = DriverManager.getConnection(URL, USERNAME, PASSWORD);
     }
     public String ReturnInfFromComponent(String component) throws SQLException {
-        String sql = "SELECT inf FROM " + TABLE + " WHERE component = ?";
-        StartConnection();
-        statement = connection.prepareStatement(sql);
-        statement.setString(1, component);
-        ResultSet resultSet = statement.executeQuery();
-        String result = "";
+        value = "select inf from " + TABLE + " where component='" + component + "';";
+        String massage="";
+        StartConnection(value);
         while (resultSet.next()) {
-            result = resultSet.getString("inf");
+            massage = resultSet.getString("inf");
         }
-        return result;
+        return massage;
     }
 }
